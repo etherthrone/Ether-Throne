3427360a5f552159585a711ec26fcab427b47f42
Stefan Rendle <goldbyol@outlook.com>
2018-03-12 19:31:32 +0100

    implemented cors

--- app-backend/server.js
+++ app-backend/server.js
@@ -1,6 +1,7 @@
 var app = require('./app');
 var port = process.env.PORT || 8080;
-
+var cors = require('cors')
+app.use(cors())
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
