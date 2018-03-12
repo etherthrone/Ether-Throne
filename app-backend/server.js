0ffea96be9b733fc1dc03388cdb8641984635dcc
Stefan Rendle <goldbyol@outlook.com>
2018-03-12 19:56:55 +0100

    update port

--- app-backend/server.js
+++ app-backend/server.js
@@ -1,7 +1,7 @@
 var app = require('./app');
-var port = process.env.PORT || 8080;
-var cors = require('cors')
-app.use(cors())
+var port = process.env.PORT || 3001;
+var cors = require('cors');
+app.use(cors());
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
